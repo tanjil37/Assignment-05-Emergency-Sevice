57bc8a91444fb699798463ece3b0ccaf576abbef
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-09-05 13:55:25 +0600

    Call button functionality added

--- js/script.js
+++ js/script.js
@@ -1,43 +1,69 @@
 
-
-// document.getElementById("heart").addEventListener("click", function(){
-//     let count = 0 
-//     count += 1;
-   
-   
-//     document.getElementById("heart-count").innerText = count;
-//     console.log(count)
-
-// })
-
+//heart count
 let count = 0;
 function increment(){
     count += 1; 
     document.getElementById("heart-count").innerText =count
 }
 
-let copy = 0;
-function copyCounter(){
-    alert("Numer copyed")
-    copy += 1;
-    document.getElementById("copy-count").innerText=copy
-}
-
-
-// document.getElementById("call-btn").ATTRIBUTE_NODE.addEventListener
 
 
 
+ 
+
+
+//Call Button
+document.querySelectorAll('.call-btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        console.log("Call btn clicked")
+        let coinCount = parseInt(document.getElementById('coin-count').innerText);
+        console.log(coinCount)
+        
+        if (coinCount < 20) {
+            alert('Not enough coins to make a call!');
+            return;
+        }
+
+        const card = event.target.closest('.card');
+        const serviceName = card.querySelector('.service-name-english').textContent;
+        const serviceNumber = card.querySelector('.hotline-number').textContent;
+        
+        alert(`Calling ${serviceName} at ${serviceNumber}`);
+        
+        // Deduct coins
+        coinCount -= 20;
+        document.getElementById('coin-count').textContent = coinCount
+
+        const historyList = document.getElementById('history-list');
+        const now =new Date();
+        const callTime = now.toLocaleTimeString();
+
+        const historyItem = document.createElement('div');
+        historyItem.innerHTML = `
+
+
+           
+
+             <div class="mt-4 bg-[#F2F2F2] flex justify-between rounded-lg p-5">
+              <div class="text-lg">
+                <h3 class="font-semibold text-black">${serviceName}</h3>
+                <p class="font-normal">${serviceNumber}</p>
+              </div>
+              <div class="text-lg font-normal">${callTime}</div>
+            </div>
+        `;
+        historyList.prepend(historyItem); 
+    });
+});
+
+
+//  Clear History
+const clearHistoryBtn = document.getElementById('clear-history-btn');
+const historyList = document.getElementById('history-list');
+
+clearHistoryBtn.addEventListener('click', function() {
+    historyList.innerHTML = '';
+});
 
 
 
-// document.querySelectorAll('.copy-btn').forEach(btn => {
-//             btn.addEventListener('click', () => {
-//                 const number = btn.dataset.number;
-//                 navigator.clipboard.writeText(number).then(() => {
-//                     alert(`Number ${number} copied to clipboard!`);
-//                     copies++;
-//                     updateNavbar();
-//                 });
-//             });
-//         });
